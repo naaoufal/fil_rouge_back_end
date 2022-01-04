1a42b65620c7664f14725b5fd0e479f954993c57
benmansour <naoufelbenmensour@gmail.com>
2022-01-04 18:56:20 +0100

    add files

--- index.js
+++ index.js
@@ -6,8 +6,34 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app)
 
+// facebook auth :
+const passport = require('passport')
+const facebookStrategy = require('passport-facebook').Strategy
+const session = require('express-session')
+
 // connect to database with mongodb:
 connectDB()
+
+// init passport :
+app.use(session({ 
+    secret : 'secretKey',
+    saveUninitialized : true,
+    resave : false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+// make facebook Strategy :
+passport.use(new facebookStrategy({
+    clientID : "1631059897234576",
+    clientSecret : "8e6a9c6402cd3dd1a7915d5da60d07bd",
+    callbackURL : "http://localhost:3001/facebook/auth",
+    profileFields : ['name', 'email', 'gender']
+}, function (token, refreshToken, profile, done) {
+    console.log(profile)
+    return done(null, profile)
+}))
 
 app.use(express.json())
 app.use(cors())
@@ -23,6 +49,26 @@ app.use("/api/comments", require("./back_end/firebase/comments/routes/comments")
 // public route for groups (testing) : 
 app.use("/api/groups", require("./back_end/routes/groups"))
 
+// Authentification using social networks :
+app.get("/facebook/auth", passport.authenticate('facebook', { scope : 'email' }))
+app.get("/facebook/callback", passport.authenticate('facebook', {
+    successRedirect : '/profile',
+    failureRedirect : '/failed'
+}))
+app.get("/profile", (req, res) => {
+    res.send("You re a valid user")
+})
+app.get("/failed", (req, res) => {
+    res.send("Failed User Request !!!")
+})
+
+// express sessions functions :
+passport.serializeUser(function (user, done) {
+    done(null, user)
+})
+passport.deserializeUser(function(id, done) {
+    return done(null, id)
+})
 
 // start the server
 server.listen(3001, () => {console.log("the server is started")});
